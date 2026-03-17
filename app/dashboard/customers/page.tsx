e2e9c001002125c8ed8e9e409186bfdc03efe44b
Martin <h012193213@gmail.com>
2026-03-17 23:56:13 +0800

    Tutorial completed

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,8 @@
+import { Metadata } from 'next/dist/lib/metadata/types/metadata-interface';
 
+export const metadata: Metadata = {
+  title: 'Customers',
+};
 export default function Page() {
   return <p>Customers Page</p>;
 }
---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import nextVitals from 'eslint-config-next/core-web-vitals';
+
+export default [
+  ...nextVitals,
+  {
+    ignores: ['.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+  },
+  {
+    rules: {
+      'react/display-name': 'off',
+    },
+  },
+];
